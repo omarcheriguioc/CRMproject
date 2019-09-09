100099e82130abc9a09c7b4adbe569712da9429c
omarcheriguioc <omar.cherigui.oc@gmail.com>
2019-09-09 17:29:52 +0200

    customer service and its implementation added

--- src/main/java/com/example/entity/Customer.java
+++ src/main/java/com/example/entity/Customer.java
@@ -2,21 +2,28 @@ package com.example.entity;
 
 import java.util.Date;
 
+//Class Customer 
 public class Customer {
+	//Customer's first name
 	private String fName;
+	//Customer's last name
 	private String lName;
+	//Customer's last name
 	private Date dateNaissance;
 	
+	//Empty constructor
 	public Customer() {
 		
 	}
 	
+	//Constructor using parameters
 	public Customer(String fName, String lName, Date dateNaissance) {
 		super();
 		this.fName = fName;
 		this.lName = lName;
 		this.dateNaissance = dateNaissance;
 	}
+	
 	public String getfName() {
 		return fName;
 	}
