6cf398d5aaf810d57f2ec4757a9924a8a9246e24
omarcheriguioc <omar.cherigui.oc@gmail.com>
2019-09-10 10:33:18 +0200

    implementation of customer service

--- src/main/java/com/example/services/CustomerService.java
+++ src/main/java/com/example/services/CustomerService.java
@@ -7,8 +7,8 @@ import com.example.entity.Customer;
 public interface CustomerService {
 	
 	public List<Customer> listCostumers();
-	public Customer addCustomer(Customer customer);
-	public boolean updateCustomer(Customer customer);
+	public boolean addCustomer(Customer customer);
+	public Customer updateCustomer(Customer customer);
 	public boolean deleteCustomer(Customer customer);
 	
 }
--- src/main/java/com/example/services/CustomerServiceImpl.java
+++ src/main/java/com/example/services/CustomerServiceImpl.java
@@ -1,33 +1,50 @@
 package com.example.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.example.entity.Customer;
 
+//Implementation of customer service
 public class CustomerServiceImpl implements CustomerService {
+	
+	// Customer's ArrayList that has all the customers
+	private List<Customer> customers = new ArrayList<Customer>();
+	
 
+	// function that lists all the customers
 	@Override
 	public List<Customer> listCostumers() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.customers;
 	}
-
+	
+	// function that adds a new customer to the list of customers
 	@Override
-	public Customer addCustomer(Customer customer) {
+	public boolean addCustomer(Customer customer) {
 		// TODO Auto-generated method stub
-		return null;
+		if(customer == null) return false;
+		boolean add = this.customers.add(customer);
+		return add;
 	}
 
+	// function that update a customer in the list of customers
 	@Override
-	public boolean updateCustomer(Customer customer) {
+	public Customer updateCustomer(Customer customer) {
 		// TODO Auto-generated method stub
-		return false;
+		if(customer == null) return null;
+		int index = this.customers.indexOf(customer);
+		Customer customerTemp = this.customers.set(index, customer);
+		return customerTemp;
 	}
-
+	
+	//function that 
 	@Override
 	public boolean deleteCustomer(Customer customer) {
 		// TODO Auto-generated method stub
-		return false;
+		if(customer == null) return false;
+		boolean remove = this.customers.remove(customer);
+		return remove;
 	}
 
 }
